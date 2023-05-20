154d77bbf263e22bec52cf862bd5ee8fb58ccd53
ACFcoding <adrianc.franks@gmail.com>
2023-05-20 17:02:51 -0600

    Made initial changes to include the characters and first few prompts. Work still needs to be done on prompt to, the input will need to be validated, and the final password displayed. Additionally, there is an issue with the error message not lopping.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password-Generator-Project-5-18-2023
+
+A site assisting the user in generating a random password meeting 
--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+// Added a function to generate the password being called further down
+function generatePassword() {
+//Remember the validate input part
+//Show the password on the page
+
+//Added variables for the potential characters and prompts for the user to answer to generate the password.
+var lengthRequest = prompt("Select a length for your password. It must be between 8 and 128 characters. Please enter the number now.")
+if (lengthRequest < 8) {
+lengthRequest = prompt("Invalid length. Try again. Length must be between 8 and 128 characters.")
+}
+else if (lengthRequest > 128) {
+  lengthRequest = prompt("Invalid length. Try again. Length must be between 8 and 128 characters.");
+}
+var characterSelection = prompt("You have the options of upper case, lower case, numeric, and/or special characters. Include one example of each that you request.")
+var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZX"
+var charactersLower = "abcdefghijklmnopqrstuvwxyz"
+var charactersNumeric = "1234567890"
+var charactersSpecial = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
