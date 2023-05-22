65264ad800b6189d88298bfb7e7c1e50cf3d4d6a
ACFcoding <adrianc.franks@gmail.com>
2023-05-22 17:25:41 -0600

    Added to the array. Still figuring out how to get the random effect

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Password-Generator-Project-5-18-2023
 
-A site assisting the user in generating a random password meeting 
+A site assisting the user in generating a random password meeting a list of user selected criteria.
+
+Video used for some assitance in remembering certain Javascript elements.
+https://www.youtube.com/watch?v=m5XF1OQ6rSg
--- script.js
+++ script.js
@@ -4,23 +4,61 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Added a function to generate the password being called further down
-function generatePassword() {
+function generatePassword(lengthRequest, charactersLower, charactersNumeric, charactersUpper, charactersSpecial) {
 //Remember the validate input part
 //Show the password on the page
 
 //Added variables for the potential characters and prompts for the user to answer to generate the password.
 var lengthRequest = prompt("Select a length for your password. It must be between 8 and 128 characters. Please enter the number now.")
 if (lengthRequest < 8) {
-lengthRequest = prompt("Invalid length. Try again. Length must be between 8 and 128 characters.")
+lengthRequest = prompt("Invalid length. Try again. Length must be between 8 and 128 characters.");
 }
 else if (lengthRequest > 128) {
   lengthRequest = prompt("Invalid length. Try again. Length must be between 8 and 128 characters.");
 }
-var characterSelection = prompt("You have the options of upper case, lower case, numeric, and/or special characters. Include one example of each that you request.")
-var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZX"
-var charactersLower = "abcdefghijklmnopqrstuvwxyz"
-var charactersNumeric = "1234567890"
-var charactersSpecial = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+// used confirm as the method of getting user criteria for password
+var characterUpperBool = confirm("Do you want upper case?")
+var characterLowerBool = confirm("Do you want lower case?")
+var characterNumericBool = confirm("Do want to use numbers?")
+var characterSpecialBool = confirm("Do you want to use special characters?")
+
+var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZXYZ".split("")
+var charactersLower = "abcdefghijklmnopqrstuvwxyz".split("")
+var charactersNumeric = "1234567890".split("")
+var charactersSpecial = "!@#$%^&*()".split("")
+//added array
+var characterArray = [];
+
+
+// Added if else statements to parse user needs regarding password.
+//Adjusted said if else statements to actually work right.
+if (characterUpperBool) {
+console.log("upper selected")
+characterArray += characterArray.concat(charactersUpper);
+return characterArray;
+}
+
+if (characterLowerBool) {
+  console.log("lower selected")
+characterArray.concat(charactersLower)
+return characterArray;
+}
+
+if (characterNumericBool) {
+console.log("numeric selected")
+characterArray.concat(charactersNumeric)
+return characterArray;
+}
+
+if (characterSpecialBool) {
+console.log("special selected")
+characterArray.concat(charactersSpecial)
+return characterArray;
+}
+else {
+  window.alert(["Invalid entry. Reload and try again."])
+}
+return characterArray;
 
 }
 
